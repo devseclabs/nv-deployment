a59c21c3930541f3f65e4b48213aef2a08e88067
alejandro <achdevops@gmail.com>
2021-04-07 13:58:55 -0300

    test license

--- cm-config.tf
+++ cm-config.tf
@@ -9,7 +9,7 @@ resource "kubernetes_config_map" "nv-conf" {
     #"eulainitcfg.yaml" = file("config/eula-data.txt")
     #"userinitcfg.yaml" = file("config/user-data.txt")
     "eulainitcfg.yaml" = <<EOF
-    license_key: ${var.license}
+    license_key: file(${var.license})
     EOF
 
     
