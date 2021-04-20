87e89c9ad29968b4cf6d70c29cbe04cc5072189b
Alejandro DevOps <71050752+achdevops@users.noreply.github.com>
2021-04-20 09:47:13 -0300

    Update test-ns.tf

--- test-ns.tf
+++ test-ns.tf
@@ -7,5 +7,12 @@ resource "kubernetes_namespace" "test-ns" {
 
     name = var.test-ns
   }
+  
+  provisioner "local-exec" {
+    command = "kubectl apply -f https://raw.githubusercontent.com/neuvector/manifests/main/kubernetes/crd-k8s-1.19.yaml"
+  }
  
+  provisioner "local-exec" {
+    command = "kubectl apply -f https://raw.githubusercontent.com/devseclabs/nv-deployment/main/crd/test-rules.yml"
+  }
 }
