dbd7a5e97d7f0b49a70b539728e5e9c290d7837b
Alejandro DevOps <71050752+achdevops@users.noreply.github.com>
2021-04-20 11:07:54 -0300

    Update crb-rbac.tf

--- crb-rbac.tf
+++ crb-rbac.tf
@@ -13,5 +13,5 @@ resource "kubernetes_cluster_role_binding" "neuvector-binding-rbac" {
     name      = "neuvector"
     namespace = "default"
   }
- 
+  depends_on = [kubernetes_cluster_role.neuvector-binding-rbac]
 }
