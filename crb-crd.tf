f642db03016e0859b91e3cb3ddf5e6fbf42cf7d4
Alejandro DevOps <71050752+achdevops@users.noreply.github.com>
2021-04-20 11:07:29 -0300

    Update crb-crd.tf

--- crb-crd.tf
+++ crb-crd.tf
@@ -13,5 +13,5 @@ resource "kubernetes_cluster_role_binding" "neuvector-binding-customresourcedefi
     name      = "neuvector"
     namespace = "default"
   }
- 
+  depends_on = [kubernetes_cluster_role.neuvector-binding-customresourcedefinition]
 }
